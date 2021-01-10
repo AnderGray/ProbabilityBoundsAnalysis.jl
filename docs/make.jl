188728f74720866af47ba49e89a806429bf09c35
Ander Gray <ander.gray@liverpool.ac.uk>
2021-01-10 00:22:34 +0000

    make package

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,21 @@ using Documenter, ProbabilityBoundsAnalysis
 
 makedocs(
     modules = [ProbabilityBoundsAnalysis],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     sitename="ProbabilityBoundsAnalysis.jl",
-    authors = "Ander Gray",
-    pages = ["Home" => "index.md"]
+    authors = "Ander Gray and Scott Ferson",
+    doctest=false,
+    pages = [
+        "Home" => "index.md",
+        "Theory" => "theory.md",
+        "User guide" => "userguide.md",
+        "Examples" => "examples.md",
+        "API" => "api.md"
+        ]
     )
+
+
+
+deploydocs(
+    repo   = "github.com/AnderGray/ProbabilityBoundsAnalysis.jl",
+)
