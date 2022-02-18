8c28dae4411af7662c12ea91542e915535e9d4c9
Ander Gray <ander.gray@liverpool.ac.uk>
2022-02-18 02:51:36 +0000

    revert changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,3 @@ using Test, ProbabilityBoundsAnalysis, IntervalArithmetic, Distributions, Docume
 include("pbox/pbox.jl")
 include("pbox/distributions.jl")
 include("pbox/arithmetic.jl")
-
-DocMeta.setdocmeta!(ProbabilityBoundsAnalysis, :DocTestSetup, :(using ProbabilityBoundsAnalysis, IntervalArithmetic, Random; Random.seed!(31415)); recursive=true)
-doctest(ProbabilityBoundsAnalysis, manual = false)
